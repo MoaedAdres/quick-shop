9856a1e50a02437e1768218411a462bc2295342e
Moaed <moaed@ugaritech.ca>
2025-08-24 23:59:13 +0300

    fix

--- src/Views/Dashboard/Profile/Profile.tsx
+++ src/Views/Dashboard/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { icons } from "@/Constants/icons";
 import { mockUser } from "@/data/mock-data";
